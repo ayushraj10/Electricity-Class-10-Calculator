1dfc7150e6159f96d01300340584220b2a106e14
Ayush Raj <64605738+ayushraj10@users.noreply.github.com>
2021-01-01 11:24:14 +0400

    Add files via upload

--- Electricity Class 10 Calculator.py
+++ Electricity Class 10 Calculator.py
@@ -8,6 +8,33 @@ if ia==1:
     iqt= iq/it
     iqtx=str(iqt)
     print(iqtx+' Amperes')
-
+if ia==2:
+    iv=int(input('Value of Voltage:'))
+    ir=int(input('Value of Resistance:'))
+    ivr=iv/ir
+    ivrx=str(ivr)
+    print(ivrx+' Amperes')
+if ia==3:
+    ip=int(input('Value of Power:'))
+    ir=int(input('Value of Resistance:'))
+    ipr=ip/ir
+    iprx= ipr**0.5
+    iprxy=str(iprx)
+    print(iprxy+' Amperes')
+if ia==4:
+    ip=int(input('Value of Power:'))
+    iv=int(input('Value of Voltage:'))
+    ipv=ip/iv
+    ipvx=str(ipv)
+    print(ipvx+' Amperes')
+if ia==5:
+    ih=int(input('Value of Heat:'))
+    ir=int(input('Value of Resistance'))
+    it=int(input('Value of Time:'))
+    irt= ir*it
+    ihrt=ih/irt
+    ihrtx=ihrt**0.5
+    ihrtxy=str(ihrtx)
+    print(ihrtxy+' Amperes')
 if a==2:
     print('So you asked Potential Difference')
